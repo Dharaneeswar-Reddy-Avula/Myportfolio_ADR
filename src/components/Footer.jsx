e26290ced695b8e8c76b4a39b233f6d31f2b8634
DharaneeswarDharaneeswar-Reddy-Avula <avuladharaniswarreddy@gmail.com>
2025-05-25 01:34:53 +0530

    Dhanu

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <div className='bg-[#02050a] font-poppins md:px-[90px]'>
       <footer className=" text-white py-12">
-  <div className="max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-3 gap-10 py-[60px]">
+  <div className="max-w-7xl mx-auto px-3 md:px-6 grid grid-cols-1 md:grid-cols-3 gap-10 py-[60px]">
     
     <div className="flex items-start space-x-4">
       <div className="bg-green-400 text-black rounded-full p-4 text-4xl">
@@ -15,7 +15,7 @@ const Footer = () => {
       </div>
       <div>
         <h4 className="text-lg font-bold mb-3">Address</h4>
-        <p className='text-[#A2A2A2] font-poppins'>2118 Thornridge Cir. Syracuse,<br />Connecticut 35624</p>
+        <p className='text-[#A2A2A2] font-poppins'>E.Ramapuram,<br />Kalasapadu (M), <br />Kadapa (D), <br />AndhraPradesh,<br />516217</p>
       </div>
     </div>
 
@@ -25,7 +25,7 @@ const Footer = () => {
       </div>
       <div>
         <h4 className="text-lg font-bold mb-3">Lets talk us</h4>
-        <p className='text-[#A2A2A2]'>(603) 555-0123<br />(603) 555-0123</p>
+        <p className='text-[#A2A2A2]'>+91 8790756930</p>
       </div>
     </div>
 
@@ -33,29 +33,31 @@ const Footer = () => {
       <div className="bg-green-400 text-black rounded-full p-4 text-4xl">
      <MdMarkEmailUnread />
       </div>
-      <div>
-        <h4 className="text-lg font-bold mb-3">Send us email</h4>
-        <p className='text-[#A2A2A2]'>deanna.curtis@example.com<br />curtis@example.com</p>
-      </div>
+  <div className=''>
+  <h4 className="text-lg font-bold mb-3">Send us email</h4>
+  <p className='text-[#A2A2A2] break-words text-md '>avuladharaniswarreddy <br className='md:hidden'/> @gmail.com</p>
+</div>
+
     </div>
   </div>
 
   <div className="border-t border-gray-700 my-8 mx-6"></div>
 
   <div className="max-w-7xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center text-gray-400">
-    <p>© CodeeFly 2024 | All Rights Reserved</p>
+    <p>© Dharaneeswar Reddy Avula 2025 | All Rights Reserved</p>
     <div className="flex space-x-6 mt-4 md:mt-0">
       <a href="#" className="hover:text-white">Terms & Conditions</a>
       <a href="#" className="hover:text-white">Privacy Policy</a>
       <a href="#" className="hover:text-white">Sitemap</a>
     </div>
     <div className="mt-4 md:mt-0">
-      <button className="text-white border border-gray-600 rounded-full w-10 h-10 flex items-center justify-center hover:bg-gray-800 transition">
+   <a href="#">  <button className="text-white border border-gray-600 rounded-full w-10 h-10 flex items-center justify-center hover:bg-gray-800 transition">
         <svg className="w-5 h-5" fill="none" stroke="currentColor" strokeWidth="2"
              viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path d="M5 15l7-7 7 7"></path>
         </svg>
       </button>
+      </a> 
     </div>
   </div>
 </footer>
